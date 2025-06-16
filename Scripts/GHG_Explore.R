eac840dbac34d62d799c856d224c9263918a8ada
Alyson Hall <78553280+alyhall@users.noreply.github.com>
2025-06-16 09:22:45 +0100

    new graphs

--- Scripts/GHG_Explore.R
+++ Scripts/GHG_Explore.R
@@ -7,7 +7,7 @@
 ## Author: Alyson Hall
 ##
 ## Date Created: 2025-06-12
-## Date Edited: 2025-06-12
+## Date Edited: 2025-06-13
 ##
 ## Copyright (c) Alyson Hall, 2025
 ## Email: achall@vims.edu
@@ -23,6 +23,7 @@
 
 library(tidyverse)
 library(readxl)
+library(fuzzyjoin)
 
 ## Load in Data ----------------------------
 
@@ -33,38 +34,55 @@ GHG <- read_excel("Data/GHG_2025-06-12.xlsx",
                 . <- .[-c(1, 2), ]  # Remove both the header row and units row
                 . 
                 } %>% 
-          select(TIME:LASER_T) # Retain only columns I want
+          select(TIME:LASER_T) %>%  # Retain only columns I want
+          mutate(TIME = hms::as_hms(TIME),
+                 TIME = hms::as_hms(TIME - 50), # Licore was ahead by 50 s
+                 CO2 = as.numeric(CO2),
+                 CH4 = as.numeric(CH4)) 
 
 
 # Read in measurements from data sheet
-# Pivot so shading are own columns called Process or something...
+
+Measure <- read_excel("Data/GHG_2025-06-12.xlsx", sheet = 2) %>% 
+                mutate(Start_Time = hms::as_hms(Start_Time),
+                       End_Time = hms::as_hms(End_Time),
+                       shade_Per = as.factor(Shade_Per))
 
 
-# Combine by second brainstorm based on my previous code 
+# Combine based on run time
 
-library(fuzzyjoin)
+Combo <- 
+    fuzzy_left_join(
+      GHG,
+      Measure,
+      by = c("TIME" = "Start_Time", "TIME" = "End_Time"),
+      match_fun = list(`>=`, `<=`)) 
 
-# Treatments with start/end times
-df_treatments <- tibble(
-  treatment = c("A", "B"),
-  start_time = hms::as_hms(c("00:00:00", "00:00:05")),
-  end_time   = hms::as_hms(c("00:00:04", "00:00:09"))
-)
+Combo <- 
+  Combo %>% 
+      mutate(Dur = difftime(Combo$TIME, Combo$Start_Time)) %>% # Create a column for runtime 
+      filter(!Dur == "NA") # Remove the NA (between measurements and before machine started)
 
-f_joined <- fuzzy_left_join(
-  df_measurements,
-  df_treatments,
-  by = c("time" = "start_time", "time" = "end_time"),
-  match_fun = list(`>=`, `<=`)
-)
+# get flux per s for each treatment
 
-
-OR
-
-# Perform a non-equi join
-df_measurements_with_treatment <- df_treatments[df_measurements, 
-                                                on = .(start_time <= time, end_time >= time), 
-                                                nomatch = 0]
+Fluxes <- 
+    Combo %>%
+      group_by(Mesocosm_Treatment, Replicate, Shade_Per) %>%
+      arrange(Mesocosm_Treatment, Replicate, Shade_Per, Dur) %>% 
+      # Generating fluxes for gar exchange per s 
+      mutate(CO2flux = (as.numeric(CO2) - lag(as.numeric(CO2), n = 1)),
+             CH4flux = (as.numeric(CH4) - lag(as.numeric(CH4), n = 1))) 
 
 
-# Once we have 
+# Data Exploration 
+
+Combo %>% 
+  filter(Dur < 241) %>% 
+  filter(Dur > 120) %>% 
+  filter(Mesocosm_Treatment == "MED") %>% 
+  ggplot(aes( x = Dur, y = CH4, color = Shade_Per)) +
+  geom_point() +
+  facet_wrap(~Replicate, scales = "free_y")
+
+
+# No MED 2 dark
