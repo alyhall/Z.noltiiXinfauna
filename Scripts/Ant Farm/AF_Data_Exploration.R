2e905240a0e7e434db61e96859994f7146f9e92a
Alyson Hall <78553280+alyhall@users.noreply.github.com>
2025-07-18 14:34:46 +0100

    To do list

--- Scripts/Ant Farm/AF_Data_Exploration.R
+++ Scripts/Ant Farm/AF_Data_Exploration.R
@@ -4,11 +4,11 @@
 ##
 ## Purpose of script: 
 ##
-## Author: Alyson Hall
+## Author: Alyson & Ashitha
 ##
 ## Date Created: 2025-07-18
 ## Date Edited: 2025-07-18
-##
+## Last Edited by: Alyson
 ##
 
 ## Notes:
@@ -27,6 +27,12 @@ library(tidyverse)
 
 ## Data Load In ----------------
 
+ImageJ <- read_excel("Data/Ant Farm/ImageJ_Analysis.xlsx", 
+                                 sheet = "ImageJ", col_names = TRUE, 
+                                 skip = 29)
 
-
-hello
+# To-Do:
+# - read in ImageJ Data
+# - read in treatment data 
+# Merge them together using a function from the join() family (Ex: left_join())
+# Create a summary boxplot for some of the metrics per species through time
