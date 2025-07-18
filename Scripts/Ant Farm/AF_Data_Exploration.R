8685bb2ed36659efdb5af34f5f97e7cba30eb85c
Alyson Hall <78553280+alyhall@users.noreply.github.com>
2025-07-18 13:59:15 +0100

    new script

---
+++ Scripts/Ant Farm/AF_Data_Exploration.R
@@ -0,0 +1,29 @@
+## Header and Preamble ---------------------------
+##
+## Script name: 
+##
+## Purpose of script: 
+##
+## Author: Alyson Hall
+##
+## Date Created: 2025-07-18
+## Date Edited: 2025-07-18
+##
+##
+
+## Notes:
+## 
+##
+##
+
+## Load up the packages we will need: 
+library(tidyverse)
+
+
+##
+## When using or reading my code, I recommended turning on code soft-wrap.
+########End
+
+
+## Data Load In ----------------
+
