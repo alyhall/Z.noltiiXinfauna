4056e26afb19257c39c98c1932fb72462130c8fc
Ashithababu <ashithababu25@gmail.com>
2025-07-24 16:52:35 +0100

    Sediment height
    sediment surface from plot profiles from t0 to get sediment height

--- Scripts/Ant Farm/AF_Data_Exploration.R
+++ Scripts/Ant Farm/AF_Data_Exploration.R
@@ -80,16 +80,93 @@ Profiles <- excel_sheets(file_path)[-1] %>%  # Skip metadata sheet
 
 # Now we need to split columns again to get Tank_Number and Image_Timepoint 
 
+Profiles <-
+Profiles %>%
+  mutate(
+    Name = str_match(Image_ID, "^([A-Z]+)_")[,2],
+    Tank_Number = str_match(Image_ID, "_([0-9A-Z]+)_")[,2],
+    Time_Point = as.numeric(str_match(Image_ID, "_([0-9]+)$")[,2]),
+    Tank_Number = ifelse(Tank_Number == "B4","4B",Tank_Number)
+  )
 
 # Then link with treatment file as above
 
+Profiles <- 
+  Treatment %>% 
+ mutate(Species = ifelse(is.na(Species), "Control",
+             Species)) %>% 
+  full_join(Profiles, 
+            by = "Tank_Number")
 
+ Profiles$Species
+#scatter plot
+
+Profiles %>% 
+  filter(Time_Point == "0") %>% 
+  ggplot(
+    aes( x = Depth,
+         y = Grey_Value,
+         colour = Tank_Number ) ) +
+  geom_point() +
+  labs(title = "scatter plot ",
+       y = "Grey Value (cm)",
+       x = "Depth(cm)" ) +
+   facet_wrap(~Species) 
 
 # Data Analysis ------------------------------------
 
 
 # Find sediment surface from plot profiles from t0 to get sediment height and use for all else 
+# Subtract empty space from plot profile, measured for each tank at t0
 
+#subset out one tank all time points
+B1 <- subset(Profiles, Profiles$Tank_Number=="1B")
+             
+#cut off space above tank for this profile all time points
+B1 <- subset(B1, B1$Depth > 12.729)
+
+#Find step length for profil1e distance for each timepoint
+B1_SL0 <- as.data.frame(B1[B1$Time_Point==0, "Depth"])[2,1] - as.data.frame(B1[B1$Time_Point==0, "Depth"])[1,1]
+B1_SL52 <- as.data.frame(B1[B1$Time_Point==52, "Depth"])[2,1] - as.data.frame(B1[B1$Time_Point==52, "Depth"])[1,1]
+B1_SL168 <- as.data.frame(B1[B1$Time_Point==168, "Depth"])[2,1] - as.data.frame(B1[B1$Time_Point==168, "Depth"])[1,1]
+B1_SL240 <- as.data.frame(B1[B1$Time_Point==240, "Depth"])[2,1] - as.data.frame(B1[B1$Time_Point==240, "Depth"])[1,1]
+B1_SL336 <- as.data.frame(B1[B1$Time_Point==336, "Depth"])[2,1] - as.data.frame(B1[B1$Time_Point==336, "Depth"])[1,1]
+
+#check which timepoint are in this profile
+unique(B1$Time_Point)
+
+#Recreate adjusted depth profile distances
+B1_AdjD_0 <- seq(0, (as.numeric(count(B1[B1$Time_Point==0,]))-1)*B1_SL0, by = B1_SL0)
+B1_AdjD_52 <- seq(0, (as.numeric(count(B1[B1$Time_Point==52,]))-1)*B1_SL52, by = B1_SL52)
+B1_AdjD_168 <- seq(0, (as.numeric(count(B1[B1$Time_Point==168,]))-1)*B1_SL168, by = B1_SL168)
+B1_AdjD_240 <- seq(0, (as.numeric(count(B1[B1$Time_Point==240,]))-1)*B1_SL240, by = B1_SL240)
+B1_AdjD_336 <- seq(0, (as.numeric(count(B1[B1$Time_Point==336,]))-1)*B1_SL336, by = B1_SL336)
+
+#Combining all adjusted depth profiles into one column
+#remember to put this timepoint back in
+B1$Adj_D <- c(B1_AdjD_0, B1_AdjD_52, B1_AdjD_168, B1_AdjD_240, B1_AdjD_336)
+
+
+#run these 13 lines of code for all profile
+
+#recombine all profiles back into one data set "Profiles2"
+
+
+
+
+
+z2<- seq(0, as.numeric(count(A1[A1$Time_Point==56,]))*0.0075, by = 0.0075)
+z3 <- seq(0, as.numeric(count(A1[A1$Time_Point==240,]))*0.0075, by = 0.0075)
+
+
+as.data.frame(A1[A1$Time_Point==336, "Depth"])[2,1] - as.data.frame(A1[A1$Time_Point==336, "Depth"])[1,1]
+
+tail(z)
+
+count(A1[A1$Time_Point==56,])
+
+
+A1[A1$Time_Point==0,]
 
 
 # Our ant farm measurements were all taken from the top of the tank so we need to subtract the the time 0 depth profiles (min) which is the sediment surface 
